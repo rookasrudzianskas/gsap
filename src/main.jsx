7f9972991d36ee7469d3ee6c6f8336303e7ac526
Rokas <rookas.rudzenskas@gmail.com>
2024-03-15 15:48:43 +0200

    sentry animation

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-//...
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
@@ -19,10 +18,10 @@ Sentry.init({
       blockAllMedia: false,
     }),
   ],
-  tracesSampleRate: 1.0, 
+  tracesSampleRate: 1.0,
   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
   replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0, 
+  replaysOnErrorSampleRate: 1.0,
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
